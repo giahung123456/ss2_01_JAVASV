db0053fedf100d2d30fa5de70008e8e6134d6634
giahung123456 <dogiahungtplc@gmail.com>
2026-05-16 11:14:03 +0700

    first commit

---
+++ src/main/java/com/example/ss2_01/Ss201Application.java
@@ -0,0 +1,13 @@
+package com.example.ss2_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss201Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ss2_01/Ss201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss2_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
